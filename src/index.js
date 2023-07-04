53be9d6aaeeaf7a13aab9e45f7c382e0675e95c1
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-07-04 18:07:08 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-console.log('Hello world');
-console.log('Again. Hello world');
+// console.log('Hello world');
+// console.log('Again. Hello world');
+
+// const postId = 1;
+
+// fetch(`http://jsonplaceholder.typicode.com/posts/${postId}`)
+// .then(response => response.json())
+// .then(posts => console.log(posts))
+// .catch(error => console.log(error))
+
+const postToAdd = {
+    title: "Serjio",
+    body: "CRUD is awesome"
+};
+
+const options = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(postToAdd),
+};
+
+
+fetch("http://jsonplaceholder.typicode.com/posts", options)
+.then(response => response.json())
+.then(posts => console.log(posts))
+.catch(error => console.log(error))
